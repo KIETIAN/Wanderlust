545febd904d367b70d082a00c5f21f967fbbf3cb
VAIBHAV KUMAR <108072133+KIETIAN@users.noreply.github.com>
2025-12-08 13:20:16 +0530

    Add files via upload

---
+++ listing.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema ({
+    title:{
+       type: String,
+       required:true
+    },
+    description:String,
+    image: {
+  filename: {
+    type: String,
+    default: "listingimage",
+  },
+  url: {
+    type: String,
+    default:
+      "https://unsplash.com/photos/a-body-of-water-with-a-city-in-the-background-cDXNSKTaXOY",
+  },
+},
+    price:Number,
+    location:String,
+    country:String,
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
