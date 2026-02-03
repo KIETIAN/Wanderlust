93687aea14fb2c6c3984ce0dc6da60602892e429
VAIBHAV KUMAR <108072133+KIETIAN@users.noreply.github.com>
2026-02-03 11:31:54 +0530

    Mongo Db Created

--- app.js
+++ app.js
@@ -1,109 +1,107 @@
-// Importing Express framework
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const port = 8081;
 const express = require("express");
-const app =express(); // create express app
+const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
+const Listing = require("./models/listning.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const Review = require("./models/review.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/reviews.js");
+const userRouter = require("./routes/user");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-// mongoDB connection
+// const MONGO_URL =
+//   "mongodb+srv://hedaumithanshu:hedaumithanshu@cluster0.om5rn.mongodb.net/wanderLust";
+// ``;
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-// calling main method to connect mongoDB
-main()
-.then(() =>
-{
-    console.log("connected to the DB");
-})
-.catch((err) =>
-{
-    console.log(err);
-})
 
-async function main()
-{
-    await mongoose.connect(MONGO_URL);
+mongoose.connection.once("open", () => {
+  console.log("DB NAME =>", mongoose.connection.name);
+  console.log("DB HOST =>", mongoose.connection.host);
+  console.log("DB PORT =>", mongoose.connection.port);
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+const sessionOptions = {
+  secret: "MysuperSecretCode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 1 day
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 1 day
+    httpOnly: true, // Helps prevent XSS attacks
+  },
+};
 
-// Route for home page
-app.get("/",(req,res) =>
-{
-    res.send("Hi, i am root");
-})
+app.use(session(sessionOptions));
+app.use(flash());
 
-// Index Route
-app.get("/listings" ,async  (req,res) =>
-{
-   const alllistings=  await Listing.find({});
-res.render("listings/index", { alllistings });
+// Authentication setup Via Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user; // Make currentUser available in all templates
+  next();
 });
 
-//New Route 
-app.get("/listings/new" , (req,res) =>
-{
-    res.render("listings/new.ejs");
-})
+app.get("/", (req, res) => {
+  res.render("listings/home.ejs");
+});
+/* ---------------------------------API ROUTES Start--------------------------------------*/
 
-// Show Route 
-app.get("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    let listing = await Listing.findById(id);
+app.use("/listings", listingsRouter);
+app.use("/listings/:id", reviewsRouter);
+app.use("/", userRouter);
 
-    res.render("listings/show.ejs", { listing });
+/* ---------------------------------API ROUTES End--------------------------------------*/
+
+app.use((err, req, res, next) => {
+  let { statuscode = 500, message = "Something went wrong" } = err;
+  res.render("listings/error.ejs", { statuscode, message });
 });
 
-// Create Route 
-app.post("/listings", async (req, res) => {
-   const newListing = new Listing(req.body.listing);
-   await newListing.save();
-   res.redirect("/listings");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-// Edit Route 
-app.get("/listings/:id/edit", async (req,res) =>
-{
-    let { id } = req.params;
-    let listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-})
 
-// Update route
-app.put("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing }, { new: true, runValidators: true });
-    res.redirect(`/listings/${id}`);
-});
-
-// Delete route
-app.delete("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-
-    console.log("Deleted:", deletedListing);
-    res.redirect("/listings");
-}); 
-
-// app.get("/testListing", async (req,res) =>
-// {
-//     let sampleListing = new Listing({
-//         title : "My New Villa",
-//         description : "By the beach",
-//         price: 1200,
-//         location: "calangute, Goa",
-//         country : "India",
-//     });
-//    await sampleListing.save();
-//    console.log("sample was saved");
-//    res.send("successful testing");
-// })
-
-//start server
-app.listen(8080);
